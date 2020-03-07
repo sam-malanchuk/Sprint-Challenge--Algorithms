ce0b7c83653f4ed996ea3bf2408ff319aa7b8269
unknown <malanchuksam@gmail.com>
2020-03-07 15:37:38 -0500

    complete sprint

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,17 +4,15 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    thCount = 0
-    if word[0] + word[1] == "th":
-        return wordL
-    return word[0] + word[1]
-
-print(count_th("Tenth"))
+    # if it's less than 2 then it can't be 'th', return 0
+    if len(word) < 2:
+        return 0
+    elif word[0] == "t" and word[1] == "h":
+        return 1 + count_th(word[2:])
+    else:
+        return count_th(word[1:])
 
 # Understand:
 # takes in single string word
 # checks how many times the string has the string 'th'
 # cannot use any loops, must use recursion
-
-# Plan:
-# 
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,36 +96,24 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        # Bubble sort
-        # self.swap_item()
-        # while self.can_move_right():
-        #     self.move_right()
-        #     if self.compare_item() == 1:
-        #         self.swap_item()
-        #         self.move_left()
-        #         self.swap_item()
-        #         self.move_right()
-        #     else:
-        #         self.move_left()
-        #         self.swap_item()
-        #         self.move_right()
-        #     self.swap_item()
-        # self.swap_item()
+        self.set_light_on()
+        # light is on until sort complete
+        while self.light_is_on():
+          self.swap_item()
+          # turn the light off when nothing left on the right to sort
+          if not self.can_move_right():
+            self.set_light_off()
+          while self.can_move_right():
+            self.move_right()
+            # swap if the one holding is larger
+            if self.compare_item() == 1:
+              self.swap_item()
+          # end the loop
+          while self.compare_item() is not None:
+            self.move_left()
 
-        # Insertion Sort
-        self.swap_item()
-        while self.can_move_right:
-            if self.can_move_left:
-                self.move_left()
-                if self.compare_item == 1:
-                    self.swap_item()
-                    self.move_right()
-                    self.swap_item()
-                    self.move_left()
-                    self.swap_item()
-            else:
-                self.move_right()
+          self.swap_item()
+          self.move_right()
 
         return None
 
